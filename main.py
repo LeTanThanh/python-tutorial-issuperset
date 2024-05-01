7c966a3c20558aeeaea2edc4999e52fb3cca7a5e
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-01 21:44:57 +0700

    Python issuperset() method examples

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 if __name__ == "__main__":
-  print("Python issuperset")
+  # Python issuperset() method examples
+
+  numbers = {1, 2, 3, 4, 5}
+  scores = {1, 2, 3}
+
+  print(numbers)
+  print(scores)
+  print(numbers.issuperset(scores))
+  print(numbers.issuperset(numbers))
+  print(scores.issuperset(numbers))
